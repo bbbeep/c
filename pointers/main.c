7480724be7663d909f36d171216ffbf25db20308
bbbeep <37920434+bbbeep@users.noreply.github.com>
2018-05-13 22:52:55 +0900

    clear things up
    change in wordings,, just to clear things up

--- pointers/main.c
+++ pointers/main.c
@@ -19,10 +19,10 @@ int main(void){
     basic_pointer = &basic_value;
 
     printf("pointer is at address %p\n", &basic_pointer);
-    printf("value at address of pointer is %c\n\n", *basic_pointer);
+    printf("value at the address basic_pointer is pointing at is %c\n\n", *basic_pointer);
 
     // More advanced use of pointers
-    // Array's are just fancy pointers
+    // Arrays are just fancy pointers
     char array[5] = {'a', 'A', 'b', 'z', 'M'};
     char *same_array = array;
     char **fancy_same_array = &same_array;
@@ -31,8 +31,9 @@ int main(void){
     printf("same_array[3] = %c, addr = %p\n", same_array[3], &same_array[3]);
     printf("**fancy_same_array[3] = %c, addr = %p\n", (*fancy_same_array)[3], &(*fancy_same_array)[3]);
 
-    // char arrays are also just a "string"
-    // of characters.
+    // all "strings" are char arrays
+    // BUT not all char arrays are strings
+    // MAIN DIFFERENCE: NULL character at the end ('\0') at the end of string
     printf("array string %s, addr = %p\n", array, &array);
     printf("same array string %s, addr = %p\n", same_array, &same_array);
     printf("fancy array string %s, addr = %p\n\n", (*fancy_same_array), &(*fancy_same_array));
